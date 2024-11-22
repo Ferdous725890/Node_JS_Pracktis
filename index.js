aededf7ecc8e0d7b497960dd0bcea4a25a260e36
ferdous725890 <ferdous725890@gmail.com>
2024-11-22 17:52:20 +0600

    Node_pracktis

---
+++ index.js
@@ -0,0 +1,8 @@
+const {getName} = require("./student");
+// const age = require("./student")
+
+// console.log(age.getAge());
+// console.log(studentOne.getName());
+// console.log(studentOne.ferdous);
+
+console.log(getName());
---
+++ student.js
@@ -0,0 +1,29 @@
+exports.getName = () =>{
+    return "Ferdous"
+};
+
+
+const getAge = ()=>{
+    return "250"
+};
+
+var ferdous = "My name is ferdous"
+
+// exports.names= ferdous
+// exports.getName = getName
+// exports.getAge = getAge
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     ferdous,
+
+// }
+
+
+git init
+git add .
+git commit -m "Node_pracktis"
+git branch -M main
+git remote add origin https://github.com/Ferdous725890/Node_JS_Pracktis.git
+git push -u origin main
